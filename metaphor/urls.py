042aca66d855d3908b9f2b35f530cad25d2faf9a
jithinrajtk <jithinrajtk@gmail.com>
2016-07-10 18:03:35 +0530

    hmm

---
+++ metaphor/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+from cart.views import HomePageView
+
+urlpatterns = patterns('',
+    url(r'^$', HomePageView.as_view(), name='home'),
+    
+)
